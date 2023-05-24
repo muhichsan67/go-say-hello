b6af0a7ced4f14989ce573c1660b2e11a5236149
Ichsan Fathurrochman <muhichsan67@gmail.comB>
2023-05-24 15:17:32 +0700

    Menambah fungsi say my name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/muhichsan67/go-say-hello
 
-go 1.19
+go 1.195
--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,7 @@ package go_say_hello
 func SayHello() string {
 	return "Hi!"
 }
+
+func SayMyName(name string) string {
+	return "Halo " + name
+}
